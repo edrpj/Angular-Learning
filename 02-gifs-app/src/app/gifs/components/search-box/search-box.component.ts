5c3a376103ceac1adfb9b762979960a409673436
edrpj <pjcodifing@gmail.com>
2023-08-15 15:31:15 +0200

    feat: gifs app

---
+++ 02-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,30 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {GifsService} from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar</h5>
+    <input
+      #txtTagInput
+      type="text"
+      class="form-control"
+      placeholder="Buscar Gifs..."
+      (keyup.enter)="searchTag()"
+    />
+  `,
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) {}
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = "";
+  }
+}
---
+++ 02-gifs-app/src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: [],
+})
+export class HomePageComponent {
+  constructor(private gifsService: GifsService) {}
+
+  get gifs(): Gif[] {
+    return this.gifsService.gifList;
+  }
+}
