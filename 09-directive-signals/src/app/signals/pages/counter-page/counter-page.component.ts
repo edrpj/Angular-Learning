74c9c5b6edde0f75a03aee641b4756239be27f39
epj <e.jimenez@zertificon.com>
2023-09-19 15:18:25 +0200

    feat: mapbox standalone components, directive-signals and auth app

--- 09-directive-signals/src/app/signals/pages/counter-page/counter-page.component.ts
+++ 09-directive-signals/src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
 })
 export class CounterPageComponent {
+  public counter = signal(10);
+  public squareCounter = computed(() => this.counter() * this.counter());
 
+  increaseBy(value: number) {
+    this.counter.update((current) => current + value);
+  }
 }
--- 09-directive-signals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 09-directive-signals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,6 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
 })
-export class PropertiesPageComponent {}
+export class PropertiesPageComponent implements OnDestroy {
+  public counter = signal(10);
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
+
+  public userChangedEffect = effect(() => {
+    console.log(`${this.user().first_name} - ${this.user().last_name}`)
+  });
+
+  ngOnDestroy(): void {
+    // this.userChangedEffect.destroy();
+  }
+
+  onFieldUpdated(field: keyof User, value: string) {
+    this.user.mutate((curret) => {
+      switch (field) {
+        case 'email':
+          curret.email = value;
+          break;
+        case 'first_name':
+          curret.first_name = value;
+          break;
+        case 'last_name':
+          curret.last_name = value;
+          break;
+        case 'id':
+          curret.id = +value;
+          break;
+      }
+    });
+  }
+}
